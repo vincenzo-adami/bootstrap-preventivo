cd446ce34e35dc888c33c7647e5727377bc1a61f
vincenzo-adami <vincenzoadami92@gmail.com>
2024-07-17 17:28:41 +0200

    ADD: array di oggetti con i tipi di lavoro ed il loro prezzo orario per la select della pagina HTML; calcolato il prezzo del preventivo prendendo i valori dall'array di oggetti

--- js/script.js
+++ js/script.js
@@ -17,10 +17,34 @@ const digitsElement = document.getElementById('digits');
 // array di codici promozionali
 const promoCodes = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24']
 
+// array di oggetti composto da tipo servizio e costo servizio
+let worksTypeArray = [
+  {
+    name: 'Backend Development',
+    priceHours: 20.50
+  },
+  {
+    name: 'Frontend Development',
+    priceHours: 15.30
+  },
+  {
+    name: 'Project Analysis',
+    priceHours: 33.60
+  }
+];
+
+// inserimento option della select nel DOM
+worksTypeArray.forEach((el) => {
+  let option = document.createElement("option");
+  option.text = el.name;
+  workTypeInputElement.add(option)
+})
+
+
 // prezzo orario dei servizi
-const backendPrice = 20.50;
-const frontendPrice = 15.30;
-const analysisPrice = 33.60;
+// const backendPrice = 20.50;
+// const frontendPrice = 15.30;
+// const analysisPrice = 33.60;
 
 // verifica promo code
 function isPromoCodeValid(code) {
@@ -48,16 +72,23 @@ formElement.addEventListener('submit', function (event) {
   let workType = workTypeInputElement.value;
   let pricePreventive;
 
-  switch (workType) {
-    case ('backend'):
-      pricePreventive = backendPrice * hoursWork;
-      break;
-    case ('frontend'):
-      pricePreventive = frontendPrice * hoursWork;
-      break
-    case ('analysis'):
-      pricePreventive = analysisPrice * hoursWork;
-      break;
+  // switch (workType) {
+  //   case ('backend'):
+  //     pricePreventive = backendPrice * hoursWork;
+  //     break;
+  //   case ('frontend'):
+  //     pricePreventive = frontendPrice * hoursWork;
+  //     break
+  //   case ('analysis'):
+  //     pricePreventive = analysisPrice * hoursWork;
+  //     break;
+  // }
+
+  let worksTypeArrayFindName = worksTypeArray.includes(workType);
+
+  if (!worksTypeArrayFindName) {
+    let priceHourseFind = worksTypeArray.filter((worksTypeArray) => worksTypeArray.name === workType);
+    pricePreventive = priceHourseFind[0].priceHours * hoursWork;
   }
 
   // applicazione sconto se valido
